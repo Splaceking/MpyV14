956011d96af2cccd0a70f3b37905b87c6cb2b4fe
Splaceking <108318814+Splaceking@users.noreply.github.com>
2022-07-05 11:00:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
+  ['6289515791535'],
+  ['6285788309825'],
   ['6287898307350', 'gempy', true]  
   
 ] 
-global.ownername = [['Gempy']]
+global.ownername = [['rarabot']]
 global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownerinsta = [['@rarabot']]
 global.mods = [] 
 global.prems = [] 
 
